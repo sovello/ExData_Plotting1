dc699de8b1775771223499cabce588b4fb5eefa3
Sovello <sovellohpmgani@gmail.com>
2014-07-13 22:16:17 +0200

    some cleanup and formatted some comments

--- plot4.R
+++ plot4.R
@@ -10,20 +10,16 @@ mySql = "SELECT * FROM file WHERE Date='1/2/2007' OR Date='2/2/2007' "
 ##Reading data into householdata (use csv2) cos this is separated by ';'
 datas <- read.csv2.sql(datafile, mySql)
 
-#datas <- datas[datas == '?' ] <- NA
-#datas <- datas[complete.cases(datas)]
+## change the Date column into date type and add a new column that has date time
 datas$Date <- as.Date(datas$Date, "%d/%m/%Y")
 Times <- paste(datas$Date,datas$Time, sep=" ")
 datas <- cbind(datas,Times)
+
+##convert the new column into character so strptime() can convert this into datetime object
 datas$Times <- as.character(datas$Times)
 datas$Times <- strptime(datas$Times, format="%Y-%m-%d %H:%M:%S")
 
-##recode all ? into NAs
-##datas[datas=="?"] <- NA
-##considerColumns <- allRows[allRows[,3] != 'Not Available', ]
-##only take complete cases
-##datas[complete.cases(datas),]
-##Create location to print the current plot
+##Prepare a png device where to plot the
 png(filename="plot4.png", width=480, height=480, units="px")
 
 par(mfrow=c(2,2))
@@ -38,11 +34,14 @@ with(datas, plot(Times, Voltage, type="l", ylab="Voltage", xlab="datetime"))
 with(datas, plot(Times, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", col="black"))
 with(datas, lines(Times, Sub_metering_2, type="l", ylab="", xlab="", col="red"))
 with(datas, lines(Times, Sub_metering_3, type="l", ylab="", xlab="", col="blue"))
+
+##set the legend
 legend("topright", 
        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
        lty=c(1,1,1), col = c("black", "red", "blue"))
 
 ##fourthplot
 with(datas, plot(Times, Global_reactive_power, type="l", xlab="datetime"))
+
 ##close the png device
 dev.off()
